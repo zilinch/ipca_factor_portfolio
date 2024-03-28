ea5d501cdbf19c35988294c60f31dea974590560
Zilin Chen <zilinchen152@gmail.com>
2024-03-28 00:37:22 -0400

    ipca helper functions

--- ipca_utils.py
+++ ipca_utils.py
@@ -1,108 +1,77 @@
-import pandas as pd
 from typing import List
-from ipca import InstrumentedPCA
+import sys
+sys.path.append('/gpfs/home/zilinchen/ipca_factor_portfolio/ipca/ipca/')
+from my_ipca import InstrumentedPCA
 
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import warnings
+warnings.filterwarnings('ignore')
 
-def IPCA_impute_w_mean(
-    df: pd.DataFrame, 
-    characteristics: List[str], 
-    threshold: int = None,
-    drop_characteristics: bool = True
+def impute_w_median(
+    df: pd.DataFrame,
+    characteristics: List
 ):
-    """
-    Impute missing values for window data used in IPCA_factor
-
-    Parameters
-    ----------
-    df:
-        Pandas dataframe to impute
-    characteristics:
-        List of the characteristics
-    threshold:
-        Threshold (in percent) of missing data to drop a characteristics
-    drop_characteristics:
-        Bool indicator for dropping characteristics when imputing. Default True.
     
-    """
-    assert len(characteristics) > 0, "List of characteristics cannot be empty"
-
-    assert not drop_characteristics or (drop_characteristics and threshold is not None), \
-                        "When drop_characteristics is True, a threshold must be provided."
-
-
-    # if drop_characteristics: remove all columns/characteristics 
-    # that have missing values greater than the threshold
-    if drop_characteristics:
-        percent_missing = df.isnull().mean() * 100
-        characteristics_to_drop = percent_missing[percent_missing > threshold].index
-        df = df.drop(columns=characteristics_to_drop)
-        characteristics_to_keep = [item for item in characteristics if item not in characteristics_to_drop]
-        characteristics = characteristics_to_keep
-
-    # fill the missing values at t with median across stock
     df_temp = df.set_index(['eom', 'id'])
-    medians_by_time = df_temp.groupby(level=0).transform('median')
-    df_imputed = df_temp.fillna(medians_by_time).reset_index()
-    df_imputed_sorted = df_imputed.sort_values(by='eom')
-
-    return df_imputed_sorted, characteristics
-
-
-
-def IPCA_factor(df, step, window_dates, current_date, characteristics, K, logger):
+    df_chars = df_temp[characteristics]
+    medians_by_time = df_chars.groupby('eom').transform('median')
+    chars_imputed = df_chars.fillna(medians_by_time)
+    df_temp.update(chars_imputed)
+    df_imputed = df_temp.reset_index()
+    return df_imputed
     
-    global lock
+
+def normalize(
+    df: pd.DataFrame,
+    characteristics: List,
+    scaler = None
+):
+    if scaler is None:
+        scaler = StandardScaler()
     
-    # Filter the data to include only rows within the selected date range
-    window_data = df[df['eom'].isin(window_dates)]
-    window_data_imputed, characteristics_to_keep = IPCA_impute_w_mean(window_data, characteristics, threshold=50)
+    def scale_characteristics(group):
+        scaler = StandardScaler()
+        group[characteristics] = scaler.fit_transform(group[characteristics])
+        return group
     
+    df_temp = df.set_index(['eom', 'id'])
+    normalized_df = df_temp.groupby(level='eom', as_index=False).apply(scale_characteristics)
+    df_new = normalized_df.reset_index(['eom', 'id'])
+    return df_new
+    
+
+
+def IPCA_factor(window_data, characteristics, K):
+    
+    # global lock
+         
     # Get the last date data from the window data
-    last_win_data = window_data_imputed[window_data_imputed['eom'] == window_dates[-1]]
-    last_ids = last_win_data['id'].unique()
+    last_date = max(window_data['eom'].values)
+    last_win_data = window_data[window_data['eom'] == last_date]
     
+    chars_to_drop = []
+    null_columns = last_win_data.columns[last_win_data.isna().any()]
+    for char in characteristics:
+        if char in null_columns:
+            chars_to_drop.append(char)
+    chars_to_keep = [item for item in characteristics if item not in chars_to_drop]
+    
+    r_t = last_win_data['ret_local_lead1m']
+    excess_r_t = last_win_data['ret_exc_lead1m']
+    X_last = last_win_data[chars_to_keep]
 
-    # Get the predict/next data but only keep stocks & characteristics appeared in last data
-    ft_data = df[df['eom'] == current_date]
-    
-    # only use characteristics that appear in the train data
-    ft_data_aligned = ft_data[last_win_data.columns]
-    ft_data_imputed, _ = IPCA_impute_w_mean(ft_data_aligned, characteristics_to_keep, drop_characteristics=False)
-    
-
-    y_next = ft_data_imputed['ret_local_lead1m']
-    X_next = ft_data_imputed[characteristics_to_keep]
-    X_last = last_win_data[characteristics_to_keep]
-    
-    assert X_next.shape[1] == X_last.shape[1], "Charateristics used for prediction has to be the same\
-                    as the characteristics used for training"
-    
     # IPCA model
-    window_data_imputed.set_index(['id', 'eom'], inplace=True)
-    y = window_data_imputed['ret_local_lead1m'] #lead return
-    X = window_data_imputed[characteristics_to_keep]
-    
-  
-    def get_ipca(X, y, K, step, date):
-        while K >= 2:
-            
-            try:
-                regr = InstrumentedPCA(n_factors=K, intercept=False, max_iter=500, iter_tol=1e-4, n_jobs= -1)
-                regr = regr.fit(X=X, y=y, quiet = True)
-                Gamma, Factors = regr.get_factors(label_ind=True)
-                return Gamma, Factors
-            except Exception as e:
-                with lock:
-                    logger.log_error(f'ValueError for ({step}, K={K}): {date}', e)
-                K -= 1
-        
-        raise ValueError("Unable to fit the model with K >= 2")
+    train_data = window_data[window_data['eom'] != last_date]
+    train_data.set_index(['id', 'eom'], inplace=True)
+    y = train_data['ret_local_lead1m'] #lead return
+    X = train_data[chars_to_keep]
     
     try:
-        Gamma, Factors = get_ipca(X, y, K, step, current_date)
-    except Exception as e:
-        with lock:
-            logger.log_error(f'ValueError for {step}: {current_date}', e)
-        raise 
+        regr = InstrumentedPCA(n_factors=K, intercept=False, max_iter=400, iter_tol=1e-4)
+        regr = regr.fit(X=X, y=y, quiet = True)
+        Gamma, Factors = regr.get_factors(label_ind=True)
+    except:
+        raise ValueError(f"Unable to fit IPCA model with K = {K}")
     
-    return Gamma, Factors, y_next, X_next, X_last, last_ids
+    return Gamma, Factors, r_t, excess_r_t, X_last
